625cdaab256154917bc54f95551e39b446966104
Jenn Cacace <jenn.cacace@gmail.com>
2018-01-14 20:07:44 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     puts array
   elsif array.length == 2 
     puts array.join("and ")
-  elsif array.length > 2 
+  else array.length > 2 
     puts array.join(", ")
   end 
 end
